15463b11c28d3732b1dfa489480039e99ba32477
matman07 <a>
2024-04-02 11:50:43 -0300

    adicionado css na tela home

--- project-ease/src/components/Home.js
+++ project-ease/src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import styles from './layout/Home.module.css'
+import fotoHome from './layout/icons/fotoHome.png'
+import {Link} from 'react-router-dom';
 
 function Home(){
     return(
-        <h1>Página principal</h1>
+        <main className={styles.conteudo}>
+        <section className={styles.conteudoPrincipal}>
+            <div className={styles.conteudoPrincipalEscrito}> 
+                <h1 className={styles.conteudoPrincipalEscritoTitulo}>Comece seu projeto no Project Ease!</h1>
+                <a1 className={styles.conteudoPrincipalEscritoSubtitulo}>Crie um projeto, selecione a categoria e adicione serviços para facilitar a organização.</a1>
+                <Link to="/criarProjeto">
+                    <button className={styles.conteudoPrincipalEscritoBotao}>Clique aqui</button>
+                </Link>
+            </div>
+                <img className={styles.conteudoPrincipalImagem} src={fotoHome} alt="fotoHome"></img>
+        </section>
+    </main>
     )
 }
 

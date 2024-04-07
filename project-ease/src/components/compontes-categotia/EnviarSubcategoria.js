50b850a329a4a8a8d6dcc49e3efee1d388adbac3
saraneves2011 <nevessara2009@gmail.com>
2024-04-07 19:39:18 -0300

    Adicionei a funcionalidade de criar novas subcategorias pra cada categoria

---
+++ project-ease/src/components/compontes-categotia/EnviarSubcategoria.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import styles from '../layout/categoria.module.css';
+
+function Modal({ onClose, id }) {
+    const [subcategoria, setSubcategoria] = useState({});
+
+    const handleCreateSubcategory = (event) => {
+        event.preventDefault();
+        console.log('AQUII'+subcategoria)
+        fetch(`http://localhost:5000/subcategoria`, {
+            method: "POST",
+            headers: { "Content-type": 'application/json' },
+            body: JSON.stringify(subcategoria)
+        })
+        .then((resp) => resp.json())
+        .then((respJson) => console.log(respJson))
+        .catch((erro) => console.log("Erro ao inserir no banco de dados"));
+    };
+    
+    const handleOnChange = (e) => {
+        setSubcategoria({ ...subcategoria,idCategoria:id, [e.target.name]: e.target.value });
+    };
+    
+
+    return (
+        <div className={styles.modalContainer}>
+            <div className={styles.modal}>
+                <div>
+                    <form id={styles.estilosForm} onSubmit={handleCreateSubcategory}>
+                        <label htmlFor="subcategoria">Insira o nome da subcategoria</label>
+                        <input onChange={handleOnChange} className={styles.input} placeholder="Insira o nome da subcategoria" type="text" name="subcategoria" id="subcategoria" />
+                        <button style={{ width: '320px', height: '40px' }}>Cadastrar subcategoria</button>
+                    </form>
+                    <button style={{ width: '320px', height: '40px' }} className="btnClose" onClick={onClose}>Cancelar</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- project-ease/src/components/compontes-categotia/mostrarCategoria.js
+++ project-ease/src/components/compontes-categotia/mostrarCategoria.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaTrash } from 'react-icons/fa';
 import styles from '../layout/categoria.module.css';
-import Modal from './ModalSubcategoria';
+import Modal from './EnviarSubcategoria';
 
-function ListaCategoria({ categoria, subcategoria }) {
+function ListaCategoria({ id, categoria }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [subcategoria, setSubcategoria] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/subcategoria', {
+            method: 'GET',
+            headers: { "Content-type": 'application/json' },
+        })
+            .then((resp) => resp.json())
+            .then((respJson) => setSubcategoria(respJson))
+            .catch((erro) => console.log("Erro ao pegar suas subcategorias: " + erro));
+    }, []);
 
     function openModal() {
         setIsModalOpen(true);
@@ -17,15 +28,31 @@ function ListaCategoria({ categoria, subcategoria }) {
     return (
         <>
             <div id={styles.divCategoria}>
-                <p>Nome: {categoria}</p>
-                <div className={styles.botaoCategoria}>
-                    <button className={styles.botaoExcluir}><FaTrash /> Excluir</button>
-                    <button className={styles.botaoExcluir} onClick={openModal}>Adicionar Subcategoria</button>
+                <div className={styles.nomeCategoria}>
+                    <p>Nome: {categoria}</p>
+                    <div className={styles.botaoCategoria}>
+                        <button className={styles.botaoExcluir}><FaTrash /> Excluir</button>
+                        <button className={styles.botaoExcluir} onClick={openModal}>Adicionar Subcategoria</button>
+                    </div>
                 </div>
+                <div className={styles.estiloSubcategorias}>
+                    {subcategoria.map((subcat) => {
+                        if (subcat.idCategoria === id) {
+                            return (
+                                <div className={styles.subcategoria}>
+                                    <p>Subcategoria: {subcat.subcategoria}</p>
+                                    <button>Excluir</button>
+                                </div>
+                            );
+                        } else {
+                            return null; 
+                        }
+                    })}
+                </div>
+                {isModalOpen && (
+                    <Modal onClose={closeModal} id={id} />
+                )}
             </div>
-            {isModalOpen && (
-                <Modal onClose={closeModal} />
-            )}
         </>
     );
 }

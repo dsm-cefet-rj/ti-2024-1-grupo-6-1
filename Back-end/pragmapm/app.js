be8e741c007d3e35c508c10703d42e5b26cf8fe1
matman07 <m.mangueira09@gmail.com>
2024-08-27 10:41:03 -0300

    iniciando login

--- Back-end/pragmapm/app.js
+++ Back-end/pragmapm/app.js
@@ -3,11 +3,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors'); // Importar CORS
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 const projetosRouter = require('./routes/projetos');
 var categoriasRouter = require('./routes/categorias');
-
 
 
 const mongoose = require('mongoose');
@@ -26,7 +27,53 @@ app.use(cors()); // Usar CORS globalmente
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
+
+app.use(session({
+    name: 'session-id',
+    secret: '12345-53291-09876-25365',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
+
+function auth (req, res, next) {
+    console.log(req.session);
+    if(!req.session.user){
+        var authHeader = req.headers.authorization;
+        if(!authHeader){
+            var err = new Error('Voce nao esta autenticado');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+            return;
+        }
+
+        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if(user == 'admin' && pass == 'password'){
+            
+            req.session.user = 'admin';
+            next();//autorizado
+        } else {
+            var err = new Error('Voce nao esta autenticado');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+        }
+    }else{
+        if(req.session.user === 'admin'){
+            next();
+        }
+        else{
+            var err = new Error('Voce nao esta autenticado');
+            err.status = 401;
+            next(err);
+        }
+    }
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -34,3 +81,4 @@ app.use('/projetos', projetosRouter);
 app.use('/categorias', categoriasRouter);
 
 module.exports = app;
+
